fa427d85eb61336c7e92c158e6cd6a61386e5016
Tomasz Rybarczyk <paluho@gmail.com>
2018-04-13 10:05:19 +0200

    Aggregate only paths..

--- test/unit/Main.purs
+++ test/unit/Main.purs
@@ -8,9 +8,12 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Timer (TIMER)
 import Control.Monad.Except (runExcept)
+import Data.Array ((:))
 import Data.Array as Array
 import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
 import Data.Foreign (Foreign, ForeignError(..), MultipleErrors, readInt, toForeign)
+import Data.Foreign.Index ((!))
 import Data.Identity (Identity(..))
 import Data.Newtype (unwrap)
 import Data.Record (insert)
@@ -21,8 +24,8 @@ import Data.Symbol (class IsSymbol)
 import Data.Tuple (Tuple(..))
 import Data.Tuple (Tuple(..))
 import Debug.Trace (traceAnyA)
-import Polyform.Input.Foreign (Attrs, IntField, NumberField, StringField, attr, object)
-import Polyform.Validation (V, Validation(..), fromEither, runValidation)
+import Polyform.Input.Foreign (Attrs, IntField, NumberField, StringField, object)
+import Polyform.Validation (V(..), Validation(..), fromEither, hoistFnMV, runValidation)
 import Test.Unit (suite, test)
 import Test.Unit.Assert (equal)
 import Test.Unit.Console (TESTOUTPUT)
@@ -34,33 +37,52 @@ data Json
   | Str (Array ForeignError)
   | Obj (Attrs Json)
 
-v fv = Validation (pure <<< fromEither <<< lmap (Array.fromFoldable) <<< runExcept <<< fv)
+
+
+type Err = { path ∷ String, error ∷ ForeignError }
+
+data Errs = Array Err
+
+v fv = Validation (pure <<< fromEither <<< lmap (map (\e → { path: [], error: [e] }) <<< Array.fromFoldable) <<< runExcept <<< fv)
+
+-- attr ∷ ∀ m. Monad m ⇒ String → Validation m _ Foreign _ → Validation m _ Foreign _
+attr name validation = hoistFnMV \i → do
+  let
+    r = runExcept (i ! name)
+    prefix n = map (\r → r { path = n : r.path })
+  case r of
+    Left error → pure $ Invalid [{ path: [name], error: Array.fromFoldable error }]
+    Right i' → do
+      r ← runValidation validation i'
+      pure $ lmap (prefix name) r
+
 
 validators =
-  { id: attr Int "id" (v readInt)
-  , nested: attr Obj "nested" $ collect
-      { field1: attr Int "field1" (v readInt)
-      , field2: attr Int "field2" (v readInt)
-      }
-  }
+  collect
+    { id: attr "id" (v readInt)
+    , nested: attr "nested" $ collect
+        { field1: attr "field1" (v readInt)
+        , field2: attr "field2" (v readInt)
+        }
+    }
 
-test1
-  :: forall m
-  . Monad m
-  => Validation
-      m
-      Json
-      Foreign
-      { nested ::
-          { field2 :: Int
-          , field1 :: Int
-          }
-      , id :: Int
-      }
-test1 = object Obj $ collect validators
-
-result1 = runValidation test1 (toForeign { id: 8, nested: { field1: "blabla" }})
-result2 = runValidation test1 (toForeign { id: 8, nested: { field1: 8, field2: 9 }})
+-- test1
+--   :: forall m
+--   . Monad m
+--   => Validation
+--       m
+--       Json
+--       Foreign
+--       { nested ::
+--           { field2 :: Int
+--           , field1 :: Int
+--           }
+--       , id :: Int
+--       }
+-- test1 = object Obj $ collect validators
+-- 
+result1 = runValidation validators (toForeign { id: 8, nested: { field1: "blabla" }})
+result2 = runValidation validators (toForeign { id: 8, nested: { field1: 8, field2: 9 }})
 
 main
   :: forall eff
