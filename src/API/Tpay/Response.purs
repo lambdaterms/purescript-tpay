b035e83a384d90553096eb9a148465ca2dce59c8
Kamirus <listopadkamil@gmail.com>
2018-10-11 15:31:48 +0200

    response migrating start

--- src/API/Tpay/Response.purs
+++ src/API/Tpay/Response.purs
@@ -3,18 +3,17 @@ module API.Tpay.Response where
 import Prelude
 
 import Control.Apply (lift2)
-import Control.Monad.Eff (Eff)
+import Data.Either (Either(..))
 import Data.Foldable (fold)
 import Data.Record.Fold (collect)
-import Data.StrMap (StrMap)
 import Data.Traversable (traverse)
-import Node.Buffer (BUFFER)
-import Node.Crypto (CRYPTO)
 import Node.Crypto.Hash (Algorithm(..), hex)
+import Polyform.Field.Validation.Combinators (check)
 import Polyform.Validation (Validation, hoistFnMV)
-import Text.Parsing.StringParser (ParseError(..))
-import Validators.Combinators (check)
-import Validators.UrlEncoded (boolean, int, number, single, urlEncoded)
+import Polyform.Validators.UrlEncoded (boolean, int, number, single, urlEncoded)
+-- import Text.Parsing.StringParser (ParseError(..))
+-- import Validators.Combinators (check)
+-- import Validators.UrlEncoded (boolean, int, number, single, urlEncoded)
 
 type ResponseBase r =
   { id :: Int
@@ -33,24 +32,24 @@ type ResponseBase r =
 type Response = ResponseBase ()
 type ResponseInternal = ResponseBase (md5sum :: String)
 
-checkMd5 
-  :: forall e
-   . String 
-  -> Validation
-      (Eff (buffer :: BUFFER, crypto :: CRYPTO | e))
-      (Array ParseError)
-      (StrMap (Array String))
-      (StrMap (Array String))
+-- checkMd5 
+--   :: forall e
+--    . String 
+--   -> Validation
+--       (Eff (buffer :: BUFFER, crypto :: CRYPTO | e))
+--       (Array ParseError)
+--       (StrMap (Array String))
+--       (StrMap (Array String))
 checkMd5 code = check msg $ lift2 (==) (single "md5sum") (str >>> calcMd5)
   where
-    msg = (const [ParseError "Invalid md5"])
+    msg = (const [Left "Invalid md5"])
     str = ((_ <> code) <<< fold) <$> traverse single ["id", "tr_id", "tr_amount", "tr_crc"]
     calcMd5 = hoistFnMV $ \x -> pure <$> hex MD5 x
 
-validateResponse
-  :: forall e
-  .  String
-  -> Validation (Eff (buffer :: BUFFER, crypto :: CRYPTO | e)) (Array ParseError) String Response
+-- validateResponse
+--   :: forall e
+--   .  String
+--   -> Validation (Eff (buffer :: BUFFER, crypto :: CRYPTO | e)) (Array ParseError) String Response
 validateResponse secret = 
       urlEncoded
   >>> checkMd5 secret
