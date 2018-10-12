636ffd6e01d5986cb033469ac4a8a02087376d68
Kamirus <listopadkamil@gmail.com>
2018-10-12 12:59:17 +0200

    fix tests

--- test/integration/Main.purs
+++ test/integration/Main.purs
@@ -4,31 +4,23 @@ import Prelude
 
 import API.Tpay.Request (prepareRequest, defaultRequest)
 import API.Tpay.Response (validateResponse)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Maybe (Maybe(..))
-import Data.Record.ShowRecord (showRecord)
-import Node.Buffer (BUFFER)
-import Node.Crypto (CRYPTO)
-import Node.HTTP (HTTP)
+import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Console (log)
 import Polyform.Validation (V(..), runValidation)
 import QuickServe (POST, RequestBody(..), quickServe)
 
-type Effs r = (console :: CONSOLE, buffer :: BUFFER, crypto :: CRYPTO | r)
-
-server2 :: forall r. RequestBody String -> POST (Effs r) String
+server2 ∷ RequestBody String → POST String
 server2 (RequestBody s) = do
-  liftEff $ log s
-  val <- liftEff $ runValidation (validateResponse "demo") s
+  liftEffect $ log s
+  val <- liftEffect $ runValidation (validateResponse "demo") s
   case val of
-    Invalid e -> liftEff $ log (show e)
-    Valid e r -> liftEff $ log (showRecord r)
+    Invalid e -> liftEffect $ log "error"
+    Valid e r -> liftEffect $ log (show r)
   pure "TRUE"
 
-main
-  :: forall eff
-  .  Eff (Effs (http :: HTTP | eff)) Unit
+main ∷ Effect Unit
 main = do
   req <- prepareRequest "" $ defaultRequest { id: 12, amount: 17.1, description: "asdf" }
   log $ show req
