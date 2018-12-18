136606afd8a80a7dcb43febf4a3550692f63b971
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-18 23:51:05 +0100

    Update to newest polyform-validators API

--- src/API/Tpay/Response.purs
+++ src/API/Tpay/Response.purs
@@ -6,17 +6,19 @@ import Control.Apply (lift2)
 import Data.Foldable (fold)
 import Data.Record.Fold (collect)
 import Data.Traversable (traverse)
-import Data.Variant (Variant, inj)
+import Data.Variant (inj)
 import Effect (Effect)
-import Foreign.Object (Object)
 import Node.Crypto.Hash (Algorithm(..), hex)
-import Polyform.Validator (Validator, hoistFnMV)
-import Polyform.Validators (check)
-import Polyform.Validators.UrlEncoded (boolean, int, number, single, urlEncoded)
+import Polyform.Validator (hoistFnMV)
+import Polyform.Validators (Validator, check)
+import Polyform.Validators.UrlEncoded (Error) as Urlencoded
+import Polyform.Validators.UrlEncoded (boolean, field, int, number, parse, string)
+import Polyform.Validators.UrlEncoded.Types (Decoded) as Urlencoded.Types
 import Type.Prelude (SProxy(..))
+import Type.Row (type (+))
 
 
-type ResponseError err = Variant (urlError ∷ String, md5 ∷ String | err)
+type ResponseError err = Urlencoded.Error + (md5 ∷ String | err)
 
 type ResponseBase r =
   { id ∷ Int
@@ -36,37 +38,37 @@ type Response = ResponseBase ()
 type ResponseInternal = ResponseBase (md5sum ∷ String)
 
 checkMd5
-  ∷ ∀ err
+  ∷ ∀ e
    . String
   -> Validator
       Effect
-      (Array (ResponseError err))
-      (Object (Array String))
-      (Object (Array String))
-checkMd5 code = check msg $ lift2 (==) (single "md5sum") (str >>> calcMd5)
+      (ResponseError + e)
+      Urlencoded.Types.Decoded
+      Urlencoded.Types.Decoded
+checkMd5 code = check msg $ lift2 (==) (field "md5sum" string) (str >>> calcMd5)
   where
     msg = (const [inj (SProxy ∷ SProxy "md5") "Invalid md5"])
-    str = ((_ <> code) <<< fold) <$> traverse single ["id", "tr_id", "tr_amount", "tr_crc"]
+    str = ((_ <> code) <<< fold) <$> traverse (flip field string) ["id", "tr_id", "tr_amount", "tr_crc"]
     calcMd5 = hoistFnMV $ \x → pure <$> hex MD5 x
 
 validateResponse
-  :: forall err
+  :: forall e
   .  String
-  -> Validator Effect (Array (ResponseError err)) String Response
+  -> Validator Effect (ResponseError + e) String Response
 validateResponse secret
-  = urlEncoded { replacePlus: true }
+  = parse { replacePlus: true }
   >>> checkMd5 secret
   >>> validators
   where
     validators = collect
-      { id: single "id" >>> int
-      , trId: single "tr_id"
-      , trDate: single "tr_date"
-      , trCrc: single "tr_crc"
-      , trAmount: single "tr_amount" >>> number
-      , trPaid: single "tr_paid" >>> number
-      , trDesc: single "tr_desc"
-      , trStatus: single "tr_status" >>> boolean
-      , trError: single "tr_error"
-      , trEmail: single "tr_email"
+      { id: field "id" int
+      , trId: field "tr_id" string
+      , trDate: field "tr_date" string
+      , trCrc: field "tr_crc" string
+      , trAmount: field "tr_amount" number
+      , trPaid: field "tr_paid" number
+      , trDesc: field "tr_desc" string
+      , trStatus: field "tr_status" boolean
+      , trError: field "tr_error" string
+      , trEmail: field "tr_email" string
       }
