02f1bc4065d316aa0846d59431d26595857fea65
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-20 09:07:03 +0100

    Bring back postBody function

--- src/Tpay.purs
+++ src/Tpay.purs
@@ -9,18 +9,28 @@ import Data.FormURLEncoded as FormURLEncoded
 import Data.Map as Map
 import Effect (Effect)
 
+-- | Excerpt from tpay documentation:
+-- | Simple integration of the merchant service with tpay.com is a redirection of the customer
+-- | to https://secure.tpay.com, along with the parameters required to define the transaction.
+-- | These parameters must be sent by POST or GET. The POST method is preferred.
+
+url ∷ String
+url = "https://secure.tpay.com"
+
 type Url = String
 
-url ∷ { code ∷ String, request ∷  Request } → Effect Url
-url { code, request } = do
-  body ← query
-  pure $ "https://secure.tpay.com?" <> body
-  where
-    query ∷ Effect String
-    query = do
-      obj ← prepareRequest code request
-      pure $ obj
-        # map Array.head
-        # Map.toUnfoldable
-        # FormURLEncoded.fromArray
-        # encode
+getUrl ∷ { code ∷ String, request ∷  Request } → Effect Url
+getUrl r = do
+  body ← postBody r
+  pure $ url <> "?" <> body
+
+type FormURLEncodedString = String
+
+postBody ∷ { code ∷ String, request ∷ Request } → Effect FormURLEncodedString
+postBody { code, request } = do
+  obj ← prepareRequest code request
+  pure $ obj
+    # map Array.head
+    # Map.toUnfoldable
+    # FormURLEncoded.fromArray
+    # encode
--- test/unit/Main.purs
+++ test/unit/Main.purs
@@ -31,11 +31,10 @@ main = runTest $ do
         query = serialize { id: "12", amount: 17.1, description: "asdf", test1: 15 }
       equal expected query
   suite "TPay" $ do
-
     let
       amount = unsafePartial (fromJust $ Decimal.fromString "17.1")
       request = defaultRequest { id: "12", amount, description: "asdf" }
       expectedUrl = "https://secure.tpay.com?accept_tos&address&amount=17.1&city&country&crc&custom_description&description=asdf&email&expiration_date&group&id=12&language&md5sum=03e6e117f7fdf42e09c47bfc089ed829&merchant_description&name&online&phone&result_email&result_url&return_error_url&return_url&timehash&zip"
     test "simple post body" $ do
-      url ← liftEffect $ TPay.url { code: "", request }
+      url ← liftEffect $ TPay.getUrl { code: "", request }
       equal expectedUrl url
