33706156249dc47f17c1f1e0441f69025a1c6c33
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-20 10:59:18 +0100

    Add reexports to main module

--- src/Tpay.purs
+++ src/Tpay.purs
@@ -1,13 +1,20 @@
-module Tpay where
+module Tpay
+  ( module Tpay.Request
+  , module Tpay.Response
+  , getUrl
+  , postBody
+  , url
+  )where
 
 import Prelude
 
-import Tpay.Request (Request, prepareRequest)
 import Data.Array as Array
 import Data.FormURLEncoded (encode)
 import Data.FormURLEncoded as FormURLEncoded
 import Data.Map as Map
 import Effect (Effect)
+import Tpay.Request (defaultRequest, Request, prepareRequest) as Tpay.Request
+import Tpay.Response (Response, validateResponse) as Tpay.Response
 
 -- | Excerpt from tpay documentation:
 -- | Simple integration of the merchant service with tpay.com is a redirection of the customer
@@ -19,16 +26,16 @@ url = "https://secure.tpay.com"
 
 type Url = String
 
-getUrl ∷ { code ∷ String, request ∷  Request } → Effect Url
+getUrl ∷ { code ∷ String, request ∷  Tpay.Request.Request } → Effect Url
 getUrl r = do
   body ← postBody r
   pure $ url <> "?" <> body
 
 type FormURLEncodedString = String
 
-postBody ∷ { code ∷ String, request ∷ Request } → Effect FormURLEncodedString
+postBody ∷ { code ∷ String, request ∷ Tpay.Request.Request } → Effect FormURLEncodedString
 postBody { code, request } = do
-  obj ← prepareRequest code request
+  obj ← Tpay.Request.prepareRequest code request
   pure $ obj
     # map Array.head
     # Map.toUnfoldable
